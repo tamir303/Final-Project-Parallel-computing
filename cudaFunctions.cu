3fb7ca8368488653a332abb6cf7e25ebfc42e5bf
linuxu <linuxu@ParallelC23-15.academic.management.afeka.local>
2023-06-09 18:46:59 +0300

    c6

--- cudaFunctions.cu
+++ cudaFunctions.cu
@@ -3,10 +3,42 @@
 #include "myProto.h"
 #include "mathCalc.h"
 
-__global__  void calcCords(int *cords) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
-    cords[i] = 
+/**
+ * Calculates the coordinates of points based on input data and user-defined functions.
+ *
+ * @param cords         Pointer to an array of Cord objects representing the coordinates (x and y) of points at different times.
+ * @param points        Pointer to an array of Point objects containing information about initial positions and coefficients.
+ * @param fPcalcX       Function pointer to a user-defined function that calculates the x coordinate.
+ *                      Signature: double functionName(double x1, double x2, double t)
+ * @param fPcalcY       Function pointer to a user-defined function that calculates the y coordinate.
+ *                      Signature: double functionName(double xCord, double a, double b)
+ */
+__global__ void calcCords(
+    Cord* cords,
+    Point* points,
+    double (*fPcalcX)(double, double, double),
+    double (*fPcalcY)(double, double, double)
+) {
+    // Get the indices of the current thread and the total number of threads
+    int point = threadIdx.x;
+    int tCount = blockIdx.x;
+    int offset = blockDim.x;
+    
+    // Retrieve the necessary values from the input arrays
+    double x1 = points[point].x1;
+    double x2 = points[point].x2;
+    double t = cords[tCount].t;
+    double xCord;
+    double a = points[point].a;
+    double b = points[point].b;
+    
+    // Calculate the x coordinate using the user-defined function
+    xCord = cords[tCount * offset + point].x = fPcalcX(x1, x2, t);
+    
+    // Calculate the y coordinate using the user-defined function
+    cords[tCount * offset + point].y = fPcalcY(xCord, a, b);
 }
+
 
 
 int computeOnGPU(Point* points, Cord* cords, int pSize, int cSize) {
