2424d1994e4f5f28eaae1b1786a94afbc7d47a0d
linuxu <linuxu@ParallelC23-11.academic.management.afeka.local>
2023-07-05 16:30:38 +0300

    m14

--- main.c
+++ main.c
@@ -45,16 +45,16 @@ int main(int argc, char *argv[]) {
    MPI_Type_commit(&MPI_INFO);
 
    // Create MPI Struct for Cord
-   int cordLen[5] = {1, 1, 1, 1 ,1};
-   MPI_Aint corddis[5] = { offsetof(Point, point), offsetof(Cord, id), offsetof(Cord, x), offsetof(Cord, y), offsetof(Cord, t)};
-   MPI_Datatype cordtypes[5] = {MPI_POINT, MPI_INT, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
-   MPI_Type_create_struct(5, cordLen, corddis, cordtypes, &MPI_CORD);
+   int cordLen[4] = {1, 1, 1 ,1};
+   MPI_Aint corddis[4] = { offsetof(Cord, point), offsetof(Cord, x), offsetof(Cord, y), offsetof(Cord, t)};
+   MPI_Datatype cordtypes[4] = {MPI_POINT, MPI_DOUBLE, MPI_DOUBLE, MPI_DOUBLE};
+   MPI_Type_create_struct(4, cordLen, corddis, cordtypes, &MPI_CORD);
    MPI_Type_commit(&MPI_CORD);
 
    // Read data and exchange information between processes
    if (rank == 0) {
       points = readPointArrayFromFile(argv[1], &info);
-      data = initCordsArray(info);
+      data = initCordsArray(info, points);
       MPI_Send(info, 1, MPI_INFO, 1, 0, MPI_COMM_WORLD);
    } else {
       info = (Info*) malloc(sizeof(Info));
@@ -62,8 +62,8 @@ int main(int argc, char *argv[]) {
    }
 
    int chunkSize = rank == size - 1 ? (int) ceil(info->tCount / size) : info->tCount / size;
-   cords = (Cord*) malloc(sizeof(Cords) * chunkSize);
-   MPI_Scatter(data, chunkSize, MPI_CORD, cords, chunkSize, MPI_CORD, 0, MPI_COMM_WORLD);
+   cords = (Cord*) malloc(sizeof(Cord) * chunkSize * info->N);
+   MPI_Scatter(data, chunkSize * info->N , MPI_CORD, cords, chunkSize * info->N, MPI_CORD, 0, MPI_COMM_WORLD);
 
    // Perform computations of Cords with CUDA and OpenMP
    /**
@@ -76,61 +76,61 @@ int main(int argc, char *argv[]) {
 
    MPI_Barrier(MPI_COMM_WORLD);
 
-   for (int i = 0; i < info->tCount * (info->N / 2); i++)
-      printf("proc: %d, pointID: %d, t: %lf, x: %lf, y: %lf\n", rank, cords[i].id, cords[i].t, cords[i].x, cords[i].y);
+   for (int i = 0; i < info->tCount / size * info->N; i++)
+      printf("proc: %d, pointID: %d, t: %lf, x: %lf, y: %lf\n", rank, cords[i].point.id, cords[i].t, cords[i].x, cords[i].y);
 
-   int chunk_size = info->tCount * info->N / size; // Number of elements in each process
-   int found_count = 0;
-   int found_flag = 0;
-   int result_flag = 0;
+   // int chunk_size = info->tCount * info->N / size; // Number of elements in each process
+   // int found_count = 0;
+   // int found_flag = 0;
+   // int result_flag = 0;
 
-   // Assuming cords is populated with data
-   // Divide the data into equal-sized chunks
-   MPI_Gather(cords, chunk_size, MPI_CORD, allCords, chunk_size, MPI_CORD, 0, MPI_COMM_WORLD);
+   // // Assuming cords is populated with data
+   // // Divide the data into equal-sized chunks
+   // MPI_Gather(cords, chunk_size, MPI_CORD, allCords, chunk_size, MPI_CORD, 0, MPI_COMM_WORLD);
 
-   // OpenMP parallel region
-   #pragma omp parallel num_threads(info->tCount)
-   {
-      // Iterate over data chunk assigned to each thread
-      for (int i = 0; i < chunk_size; i++) {
-         // Iterate over K cords for each t
-         for (int j = 0; j < info->K; j++) {
-               // Check distance condition
-               if (arePointsInDistance(cords[i].x, cords[i].y, cords[j].x, cords[j].y, info->D)) {
-                  // Set flag indicating a point has been found
-                  found_flag = 1;
-               }
-         }
-      }
-   }
+   // // OpenMP parallel region
+   // #pragma omp parallel num_threads(info->tCount)
+   // {
+   //    // Iterate over data chunk assigned to each thread
+   //    for (int i = 0; i < chunk_size; i++) {
+   //       // Iterate over K cords for each t
+   //       for (int j = 0; j < info->K; j++) {
+   //             // Check distance condition
+   //             if (arePointsInDistance(cords[i].x, cords[i].y, cords[j].x, cords[j].y, info->D)) {
+   //                // Set flag indicating a point has been found
+   //                found_flag = 1;
+   //             }
+   //       }
+   //    }
+   // }
 
-   // Communicate results to the master process
-   if (rank != 0) {
-      // Send the flag to the master process with tag 0
-      MPI_Send(&found_flag, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-   } else {
-      // Receive flags from other processes
-      for (int i = 1; i < size; i++) {
-         // Receive the flag with tag i
-         MPI_Recv(&result_flag, 1, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-         if (result_flag == 1) {
-               found_count++;
-               if (found_count >= 3) {
-                  // Send termination signal to all processes
-                  for (int j = 0; j < size; j++) {
-                     MPI_Send(&found_flag, 1, MPI_INT, j, size + 1, MPI_COMM_WORLD);
-                  }
-                  break;
-               }
-         }
-      }
-   }
+   // // Communicate results to the master process
+   // if (rank != 0) {
+   //    // Send the flag to the master process with tag 0
+   //    MPI_Send(&found_flag, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+   // } else {
+   //    // Receive flags from other processes
+   //    for (int i = 1; i < size; i++) {
+   //       // Receive the flag with tag i
+   //       MPI_Recv(&result_flag, 1, MPI_INT, i, i, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+   //       if (result_flag == 1) {
+   //             found_count++;
+   //             if (found_count >= 3) {
+   //                // Send termination signal to all processes
+   //                for (int j = 0; j < size; j++) {
+   //                   MPI_Send(&found_flag, 1, MPI_INT, j, size + 1, MPI_COMM_WORLD);
+   //                }
+   //                break;
+   //             }
+   //       }
+   //    }
+   // }
 
    // Terminate if termination signal received
-   if (found_count >= 3) {
-      MPI_Finalize();
-      return 0;
-   }
+   // if (found_count >= 3) {
+   //    MPI_Finalize();
+   //    return 0;
+   // }
 
 
    MPI_Type_free(&MPI_POINT);
